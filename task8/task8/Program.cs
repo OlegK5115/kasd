741cf038052ea115cfe812122653808713756646
OlegK5115 <olegk5115@gmail.com>
2024-11-09 13:17:50 +0300

    task 8 completed

---
+++ task8/task8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<string> stack = new MyStack<string>();
+
+            stack.push("Hello");
+            stack.push("Oleg");
+            stack.pop();
+
+            Console.WriteLine($"{stack.peek()}");
+            Console.ReadLine();
+        }
+    }
+}
