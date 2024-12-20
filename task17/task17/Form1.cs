93117961af4397f8a17ef6ee5848dbc26a62f7d5
OlegK5115 <olegk5115@gmail.com>
2024-12-20 22:40:33 +0300

    task 17 completed

---
+++ task17/task17/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace task17
+{
+    public partial class Form1 : Form
+    {
+        int operationIndex = 0;
+        String[] names = { "Add (value)", "Get (value)", "Set (index, value)", "Add (index, value)", "Remove (index)" };
+        string name;
+        
+        Color color1, color2;
+        Testing test;
+        public Form1()
+        {
+            InitializeComponent();
+            test = new Testing();
+            color1 = Color.PowderBlue;
+            color2 = Color.PaleGreen;
+        }
+
+        private void DrawGraph(int operationIndex)
+        {
+            GraphPane graphPane = zedGraphControl1.GraphPane;
+            graphPane.CurveList.Clear();
+            graphPane.Title.Text = name;
+            graphPane.XAxis.Title.Text = "Ось X";
+            graphPane.YAxis.Title.Text = "Ось Y";
+
+            double[] X = { 100, 1000, 10000, 100000 };
+            double[] Y1 = new double[4];
+            double[] Y2 = new double[4];
+
+            switch (operationIndex)
+            {
+                case 0:
+                    {
+                        Y1 = test.TestAddArray();
+                        Y2 = test.TestAddLinked();
+
+                        break;
+                    }
+                case 1:
+                    {
+                        Y1 = test.TestGetArray();
+                        Y2 = test.TestGetLinked();
+
+                        break;
+                    }
+                case 2:
+                    {
+                        Y1 = test.TestSetArray();
+                        Y2 = test.TestSetLinked();
+
+                        break;
+                    }
+                case 3:
+                    {
+                        Y1 = test.TestAddValueArray();
+                        Y2 = test.TestAddValueLinked();
+
+                        break;
+                    }
+                case 4:
+                    {
+                        Y1 = test.TestRemoveArray();
+                        Y2 = test.TestRemoveLinked();
+
+                        break;
+                    }
+
+            }
+            graphPane.AddCurve("MyArrayList", X, Y1, color1);
+            graphPane.AddCurve("MyLinkedList", X, Y2, color2);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            operationIndex = comboBox1.SelectedIndex;
+            name = comboBox1.SelectedText;
+            DrawGraph(operationIndex);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ task17/task17/Testing.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task17
+{
+    internal class Testing
+    {
+
+        MyArrayList<int> arrayList;
+        MyLinkedList<int> linkedList;
+        int minSize = 100;
+        int maxSize = 100000 + 1;
+
+        public Testing()
+        {
+            arrayList = new MyArrayList<int>();
+            linkedList = new MyLinkedList<int>();
+        }
+
+        public double[] TestAddArray()
+        {
+            double[] time = new double[4];
+            Stopwatch sw = new Stopwatch();
+            int index = 0;
+
+            for (int n = minSize; n < maxSize; n *= 10)
+            {
+                for (int i = 0; i != 20; i++)
+                {
+                    sw.Start();
+                    for (int j = 0; j != n; j++)
+                    {
+                        arrayList.Add(1);
+                    }
+                    sw.Stop();
+                    if (i != 19)
+                        linkedList.clear();
+                    time[index] = sw.ElapsedMilliseconds;
+                }
+                time[index] /= 20;
+                index++;
+            }
+            return time;
+        }
+        public double[] TestAddLinked()
+        {
+            double[] time = new double[4];
+            Stopwatch sw = new Stopwatch();
+            int index = 0;
+
+            for (int n = minSize; n < maxSize; n *= 10)
+            {
+                for (int a = 0; a != 20; a++)
+                {
+                    sw.Start();
+                    for (int i = 0; i != n; i++)
+                    {
+                        linkedList.add(1);
+                    }
+                    sw.Stop();
+                    if (a != 19)
+                        linkedList.clear();
+                    time[index] = sw.ElapsedMilliseconds;
+                }
+                time[index] /= 20;
+                index++;
+            }
+            return time;
+        }
+
+        public double[] TestGetArray()
+        {
+            TestAddArray();
+
+            double[] time = new double[4];
+            Stopwatch sw = new Stopwatch();
+            int index = 0;
+
+            for (int n = minSize; n < maxSize; n *= 10)
+            {
+                for (int a = 0; a != 20; a++)
+                {
+                    sw.Start();
+                    for (int i = 0; i != n; i++)
+                    {
+                        var c = arrayList[i];
+                    }
+                    sw.Stop();
+                    time[index] = sw.ElapsedMilliseconds;
+                }
+                time[index] /= 20;
+                index++;
+            }
+            return time;
+        }
+        public double[] TestGetLinked()
+        {
+            TestAddLinked();
+
+            double[] time = new double[4];
+            Stopwatch sw = new Stopwatch();
+            int index = 0;
+
+            for (int n = minSize; n < maxSize; n *= 10)
+            {
+                for (int a = 0; a != 20; a++)
+                {
+                    sw.Start();
+                    for (int i = 0; i != n; i++)
+                    {
+                        linkedList.get(i);
+                    }
+                    sw.Stop();
+                    time[index] = sw.ElapsedMilliseconds;
+                }
+                time[index] /= 20;
+                index++;
+            }
+            return time;
+        }
+        public double[] TestSetArray()
+        {
+            double[] time = new double[4];
+            Stopwatch sw = new Stopwatch();
+            int index = 0;
+
+            for (int n = minSize; n < maxSize; n *= 10)
+            {
+                for (int a = 0; a != 20; a++)
+                {
+                    sw.Start();
+                    for (int i = 0; i != n; i++)
+                    {
+                        arrayList[i] = 2;
+                    }
+                    sw.Stop();
+                    time[index] = sw.ElapsedMilliseconds;
+                }
+                time[index] /= 20;
+                index++;
+            }
+            return time;
+        }
+        public double[] TestSetLinked()
+        {
+            double[] time = new double[4];
+            Stopwatch sw = new Stopwatch();
+            int index = 0;
+
+            for (int n = minSize; n < maxSize; n *= 10)
+            {
+                for (int a = 0; a != 20; a++)
+                {
+                    sw.Start();
+                    for (int i = 0; i != n; i++)
+                    {
+                        linkedList.set(i, 2);
+                    }
+                    sw.Stop();
+                    time[index] = sw.ElapsedMilliseconds;
+                }
+                time[index] /= 20;
+                index++;
+            }
+            return time;
+        }
+        public double[] TestAddValueArray()
+        {
+            double[] time = new double[4];
+            Stopwatch sw = new Stopwatch();
+            int index = 0;
+            arrayList.Clear();
+
+            for (int n = minSize; n < maxSize; n *= 10)
+            {
+                for (int a = 0; a != 20; a++)
+                {
+                    sw.Start();
+                    for (int i = 0; i != n; i++)
+                    {
+                        arrayList.Add(3);
+                    }
+                    sw.Stop();
+                    if (a != 19)
+                        linkedList.clear();
+                    time[index] = sw.ElapsedMilliseconds;
+                }
+                time[index] /= 20;
+                index++;
+            }
+            return time;
+        }
+        public double[] TestAddValueLinked()
+        {
+            double[] time = new double[4];
+            Stopwatch sw = new Stopwatch();
+            int index = 0;
+            linkedList.clear();
+
+            for (int n = minSize; n < maxSize; n *= 10)
+            {
+                for (int a = 0; a != 20; a++)
+                {
+                    sw.Start();
+                    for (int i = 0; i != n; i++)
+                    {
+                        linkedList.add(i, 3);
+                    }
+                    sw.Stop();
+                    if (a != 19)
+                        linkedList.clear();
+                    time[index] = sw.ElapsedMilliseconds;
+                }
+                time[index] /= 20;
+                index++;
+            }
+            return time;
+        }
+
+        public double[] TestRemoveArray()
+        {
+            TestAddArray();
+
+            double[] time = new double[4];
+            Stopwatch sw = new Stopwatch();
+            int index = 0;
+
+            for (int n = minSize; n < maxSize; n *= 10)
+            {
+                for (int a = 0; a != 20; a++)
+                {
+                    sw.Start();
+                    for (int i = 0; i != n; i++)
+                    {
+                        arrayList.Remove(i);
+                    }
+                    sw.Stop();
+                    time[index] = sw.ElapsedMilliseconds;
+                }
+                time[index] /= 20;
+                index++;
+            }
+            return time;
+        }
+        public double[] TestRemoveLinked()
+        {
+            TestAddLinked();
+
+            double[] time = new double[4];
+            Stopwatch sw = new Stopwatch();
+            int index = 0;
+
+            for (int n = minSize; n < maxSize; n *= 10)
+            {
+                for (int a = 0; a != 20; a++)
+                {
+
+                    sw.Start();
+                    for (int i = 0; i != n; i++)
+                    {
+                        linkedList.remove(i);
+                    }
+                    sw.Stop();
+                    time[index] = sw.ElapsedMilliseconds;
+                }
+                time[index] /= 20;
+                index++;
+            }
+            return time;
+        }
+    }
+}
