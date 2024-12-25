ba3d8e5dea78e67376ba70b6dc40c8342f6e3c00
OlegK5115 <olegk5115@gmail.com>
2024-12-25 18:57:30 +0300

    task 25 completed

---
+++ task25/task25/MyHashMap.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace task25
+{
+    public class MyHashMap<TK, TV>
+    {
+        private const int DEFAULT_CAPACITY = 16;
+        private const float DEFAULT_COEFFICENT = 0.75f;
+
+        private Entry<TK, TV>[] table;
+        private int size;
+
+        private float loadFactor;
+        private int threshold;
+
+        public MyHashMap() : this(DEFAULT_CAPACITY, DEFAULT_COEFFICENT) { }
+        public MyHashMap(int initialCapacity) : this(initialCapacity, DEFAULT_COEFFICENT) { }
+        public MyHashMap(int initialCapacity, float loadFactor)
+        {
+            if (initialCapacity < 1) throw new ArgumentOutOfRangeException("initialCapacity");
+            if (loadFactor <= 0) throw new ArgumentOutOfRangeException("loadFactor");
+
+            this.loadFactor = loadFactor;
+            table = new Entry<TK, TV>[initialCapacity];
+            threshold = (int)(initialCapacity * loadFactor);
+            size = 0;
+        }
+        private int ToHash(object key)
+        {
+            if (key != null) return (key.GetHashCode() & 0x7FFFFFFF) % table.Length;
+            else throw new NullReferenceException("Key can't be null");
+        }
+        private Entry<TK, TV> GetEntry(object key)
+        {
+            Entry<TK, TV> mappedEntry = table[ToHash(key)];
+
+            while (mappedEntry != null)
+            {
+                if (mappedEntry.Key.Equals(key)) return mappedEntry;
+
+                mappedEntry = mappedEntry.Next;
+            }
+
+            return null;
+        }
+
+        public bool IsEmpty() => size == 0;
+
+        public TV Get(object key)
+        {
+            Entry<TK, TV> currentEntry = GetEntry(key);
+            if (currentEntry != null) return currentEntry.Value;
+            return default;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(table, 0, table.Length);
+            size = 0;
+        }
+
+        public bool ContainsKey(object key) => GetEntry(key) != null;
+
+        public bool ContainsValue(object value)
+        {
+            foreach (Entry<TK, TV> entry in table)
+            {
+                Entry<TK, TV> currentEntry = entry;
+                while (currentEntry != null)
+                {
+                    if (currentEntry.Value != null && currentEntry.Value.Equals(value)) return true;
+                    currentEntry = currentEntry.Next;
+                }
+            }
+
+            return false;
+        }
+
+        public HashSet<TK> KeySet()
+        {
+            HashSet<TK> hashSet = new HashSet<TK>();
+            foreach (Entry<TK, TV> entry in table)
+            {
+                Entry<TK, TV> current = entry;
+                while (current != null)
+                {
+                    hashSet.Add(current.Key);
+                    current = current.Next;
+                }
+            }
+            return hashSet;
+        }
+
+        public HashSet<KeyValuePair<TK, TV>> EntrySet()
+        {
+            HashSet<KeyValuePair<TK, TV>> hashSet = new HashSet<KeyValuePair<TK, TV>>();
+            foreach (Entry<TK, TV> entry in table)
+            {
+                Entry<TK, TV> current = entry;
+                while (current != null)
+                {
+                    hashSet.Add(new KeyValuePair<TK, TV>(current.Key, current.Value));
+                    current = current.Next;
+                }
+            }
+            return hashSet;
+        }
+
+        private void Resize()
+        {
+            int newCapacity = table.Length * 2;
+            Entry<TK, TV>[] newTable = new Entry<TK, TV>[newCapacity];
+
+            foreach (var entry in table)
+            {
+                var current = entry;
+                while (current != null)
+                {
+                    int newIndex = (current.Key.GetHashCode() & 0x7FFFFFFF) % newTable.Length;
+                    Entry<TK, TV> next = current.Next;
+
+                    current.Next = newTable[newIndex];
+                    newTable[newIndex] = current;
+
+                    current = next;
+                }
+            }
+
+            table = newTable;
+            threshold = (int)(newCapacity * loadFactor);
+        }
+
+        public void Put(TK key, TV value)
+        {
+            if (size + 1 > threshold) Resize();
+            Entry<TK, TV> currentEntry = table[ToHash(key)];
+            if (currentEntry != null)
+            {
+                while (currentEntry.Next != null)
+                {
+                    if (currentEntry.Key.Equals(key))
+                    {
+                        currentEntry.Value = value;
+                        return;
+                    }
+                    currentEntry = currentEntry.Next;
+
+                }
+
+                currentEntry.Next = new Entry<TK, TV>(key, value);
+            }
+            else
+            {
+                var hash = ToHash(key);
+                table[hash] = new Entry<TK, TV>(key, value);
+            }
+            size++;
+        }
+
+        public Entry<TK, TV> Remove(object key)
+        {
+            int index = ToHash(key);
+            Entry<TK, TV> current = table[index];
+            Entry<TK, TV> prev = null;
+            while (current != null)
+            {
+                if (current.Key.Equals(key))
+                {
+                    if (prev == null) table[index] = current.Next;
+                    else prev.Next = current.Next;
+                    size--;
+                    return current;
+                }
+
+                prev = current;
+                current = current.Next;
+            }
+            return null;
+        }
+
+        public int Size() => size;
+    }
+}
---
+++ task25/task25/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task25
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string input = "../../input.txt";
+            List<string> linesWithSpaces = ReadLinesWithSpaces(input);
+
+            // печать элем-в множества
+            foreach (var line in linesWithSpaces.ToArray()) { Console.WriteLine(line); }
+
+            Console.ReadLine();
+        }
+
+        private static List<string> ReadLinesWithSpaces(string filePath)
+        {
+            var input = new MyHashSet<string>();
+            // чтение строк файла
+            foreach (var line in File.ReadLines(filePath))
+            {
+                if (line.Contains(' ')) // проверка наличие пробелов
+                {
+                    input.Add(line.Trim());
+                }
+            }
+
+            var linesWithSpaces = input.ToArray().ToList();
+            // сортировка строк по наим. слову строки 
+            linesWithSpaces.Sort((line1, line2) => CompareLines(line1, line2));
+
+            return linesWithSpaces;
+        }
+
+        private static int CompareLines(string line1, string line2)
+        {
+            // разделяем строки на слова и сортируем их по длине
+            var words1 = line1.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).OrderBy(word => word.Length).ToList();
+            var words2 = line2.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).OrderBy(word => word.Length).ToList();
+
+            int minCount = Math.Min(words1.Count, words2.Count);
+
+            // сравниваем слова, пока найдем несовпадение
+            for (int i = 0; i < minCount; i++)
+            {
+                int lengthComparison = words1[i].Length.CompareTo(words2[i].Length);
+                if (lengthComparison != 0)
+                {
+                    return lengthComparison;
+                }
+            }
+
+            // если одна строка имеет меньше слов, то она "меньше"
+            return words1.Count.CompareTo(words2.Count);
+        }
+    }
+}
