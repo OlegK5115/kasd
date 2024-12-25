37e4776dc4235bd253c8345add6653f064ed1a5d
OlegK5115 <olegk5115@gmail.com>
2024-12-25 19:01:31 +0300

    task 27 completed

---
+++ task27/task27/MyIterator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task27
+{
+    public interface MyIterator<T>
+    {
+        bool HasNext();
+        T Next();
+        void Remove();
+    }
+
+    public interface MyIterator_ext<T> : MyIterator<T>
+    {
+        bool HasPrevious();
+
+        T Previous();
+
+        int NextIndex();
+
+        int PreviousIndex();
+
+        void Set(T element);
+
+        void Add(T element);
+    }
+}
---
+++ task27/task27/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task27
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = Enumerable.Range(1, 10).ToArray();
+
+            MyTreeSet<int> treeSet = new MyTreeSet<int>();
+            treeSet.AddAll(a);
+            MyHashSet<int> hashSet = new MyHashSet<int>(a);
+            MyPriorityQueue<int> priorityQueue = new MyPriorityQueue<int>(a);
+            MyArrayDeque<int> arrayDeque = new MyArrayDeque<int>(a);
+
+            Console.WriteLine("treeSet: ");
+            PrintCollection(treeSet.Iterator());
+            Console.WriteLine("hashSet: ");
+            PrintCollection(hashSet.Iterator());
+            Console.WriteLine("priorityQueue: ");
+            PrintCollection(priorityQueue.Iterator());
+            Console.WriteLine("arrayDeque: ");
+            PrintCollection(arrayDeque.Iterator());
+
+            MyArrayList<int> arrayList = new MyArrayList<int>(a);
+            MyVector<int> vector = new MyVector<int>(a);
+            MyLinkedList<int> linkedList = new MyLinkedList<int>(a);
+            Console.WriteLine("arrayList: ");
+            PrintCollection(arrayList.ListIterator());
+            Console.WriteLine("arrayList: ");
+            PrintCollection(vector.ListIterator());
+            Console.WriteLine("arrayList: ");
+            PrintCollection(linkedList.ListIterator());
+
+            Console.ReadLine();
+        }
+
+        static void PrintCollection<T>(MyIterator<T> myIterator)
+        {
+            while (myIterator.HasNext())
+            {
+                Console.Write($"{myIterator.Next()} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
