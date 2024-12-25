068d93aba39779572b6ad2ef0288f28f839fe098
OlegK5115 <olegk5115@gmail.com>
2024-12-25 18:49:23 +0300

    task 21 completed

---
+++ task21/task21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTreeMap<int, string> treeMap = new MyTreeMap<int, string>();
+            foreach (var item in Enumerable.Range(1, 20))
+            {
+                treeMap.Put(item * 2, $"Value {item * 2}");
+            }
+            foreach (var item in Enumerable.Range(1, 20))
+            {
+                treeMap.Put(item, $"Value {item}");
+            }
+
+            treeMap.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
