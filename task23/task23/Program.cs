de0239a05d39e292f5b24975e39ca2d782574411
OlegK5115 <olegk5115@gmail.com>
2024-12-25 18:55:06 +0300

    task 23 completed

---
+++ task23/task23/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyHashSet<int> set = new MyHashSet<int>();
+
+            set.AddAll(Enumerable.Range(1, 10).ToArray());
+            set.Remove(3);
+            set.Add(5);
+            set.Add(5);
+
+            int[] newArray = set.ToArray();
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.Write(newArray[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
